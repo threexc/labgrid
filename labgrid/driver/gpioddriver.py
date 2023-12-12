6a79f1d7114e714fe9b60d61cd736311e37fa8b0
Trevor Gamblin <tvgamblin@gmail.com>
2023-12-12 21:37:00 +0000

    add basic support for gpiod driver
    Add Jerome's preliminary work to use as a baseline.
    
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

---
+++ labgrid/driver/gpioddriver.py
@@ -0,0 +1,45 @@
+"""All GPIO-related drivers"""
+import attr
+
+from ..factory import target_factory
+from ..protocol import DigitalOutputProtocol
+from ..resource.remote import NetworkGpiodGPIO
+from ..step import step
+from .common import Driver
+from ..util.agentwrapper import AgentWrapper
+
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class GpiodDigitalOutputDriver(Driver, DigitalOutputProtocol):
+
+    bindings = {
+        "gpio": {"GpiodGPIO", "NetworkGpiodGPIO"},
+    }
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.wrapper = None
+
+    def on_activate(self):
+        if isinstance(self.gpio, NetworkGpiodGPIO):
+            host = self.gpio.host
+        else:
+            host = None
+        self.wrapper = AgentWrapper(host)
+        self.proxy = self.wrapper.load('gpiodgpio')
+
+    def on_deactivate(self):
+        self.wrapper.close()
+        self.wrapper = None
+        self.proxy = None
+
+    @Driver.check_active
+    @step(args=['status'])
+    def set(self, status):
+        self.proxy.set(self.gpio.gpiochip, self.gpio.index, status)
+
+    @Driver.check_active
+    @step(result=True)
+    def get(self):
+        return self.proxy.get(self.gpio.gpiochip, self.gpio.index)
