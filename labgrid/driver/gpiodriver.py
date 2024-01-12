b89bd7fdb4123185859afbe4fc9b3cab8b1bcaa9
Trevor Gamblin <tgamblin@baylibre.com>
2024-01-12 16:35:56 -0500

    gpiodriver: add GpiodDigitalOutputDriver
    Signed-off-by: Trevor Gamblin <tgamblin@baylibre.com>

--- labgrid/driver/gpiodriver.py
+++ labgrid/driver/gpiodriver.py
@@ -3,7 +3,7 @@ import attr
 
 from ..factory import target_factory
 from ..protocol import DigitalOutputProtocol
-from ..resource.remote import NetworkSysfsGPIO
+from ..resource.remote import NetworkSysfsGPIO, NetworkGpiodGPIO
 from ..step import step
 from .common import Driver
 from ..util.agentwrapper import AgentWrapper
@@ -43,3 +43,38 @@ class GpioDigitalOutputDriver(Driver, DigitalOutputProtocol):
     @step(result=True)
     def get(self):
         return self.proxy.get(self.gpio.index)
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class GpiodDigitalOutputDriver(Driver, DigitalOutputProtocol):
+
+    bindings = {
+        "gpio": {"GpiodGPIO", "NetworkGpiodGPIO"},
+    }
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.wrapper = None
+
+    def on_activate(self):
+        if isinstance(self.gpio, NetworkGpiodGPIO):
+            host = self.gpio.host
+        else:
+            host = None
+        self.wrapper = AgentWrapper(host)
+        self.proxy = self.wrapper.load('gpiodgpio')
+
+    def on_deactivate(self):
+        self.wrapper.close()
+        self.wrapper = None
+        self.proxy = None
+
+    @Driver.check_active
+    @step(args=['status'])
+    def set(self, status):
+        self.proxy.set(self.gpio.gpiochip, self.gpio.index, status)
+
+    @Driver.check_active
+    @step(result=True)
+    def get(self):
+        return self.proxy.get(self.gpio.gpiochip, self.gpio.index)
